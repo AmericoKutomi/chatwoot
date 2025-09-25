80c81516e54f4009c4cbd62b6d78041441e6eefd
AmericoKutomi <americo@embra.com.br>
2025-09-25 12:14:15 -0300

    timeout = 60

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,7 +1,7 @@
 require 'rack-timeout'
 
 # Define o timeout máximo para requisições (em segundos)
-Rack::Timeout.service_timeout = 60  # aumenta de 15s para 60s
+Rack::Timeout.timeout = 60  # aumenta de 15s para 60s
 
 # Reduce noise by filtering state=ready and state=completed which are logged at INFO level
 Rails.application.config.after_initialize do
